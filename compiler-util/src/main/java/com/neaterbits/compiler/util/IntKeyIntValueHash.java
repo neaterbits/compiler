ca97bb46eb938eab5c8ce797f7374054d261de42
Nils Henrik Lorentzen <nils@erus.no>
2020-04-11 18:05:29 +0200

    IntKeyIntValueHash

---
+++ compiler-util/src/main/java/com/neaterbits/compiler/util/IntKeyIntValueHash.java
@@ -0,0 +1,31 @@
+package com.neaterbits.compiler.util;
+
+public final class IntKeyIntValueHash {
+
+    private long [] hash;
+    
+    public IntKeyIntValueHash(int initialSize) {
+
+        this.hash = Hash.makeHashMap(initialSize, Hash.UNDEF);
+    }
+    
+    public void put(int key, int value) {
+        
+        this.hash = Hash.hashStore(hash, key, value, Hash.UNDEF, Hash.INT_KEY_INT_VALUE);
+    }
+    
+    public int get(int key) {
+        
+        final long value = Hash.hashGet(hash, key, Hash.UNDEF, Hash.INT_KEY_INT_VALUE);
+        
+        if (value > Integer.MAX_VALUE) {
+            throw new IllegalStateException();
+        }
+        
+        if (value < Integer.MIN_VALUE) {
+            throw new IllegalStateException();
+        }
+        
+        return (int)value;
+    }
+}
