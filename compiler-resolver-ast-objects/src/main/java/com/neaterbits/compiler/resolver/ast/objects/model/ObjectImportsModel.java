7a2658924bc37c1e64f463ce863abc7b6edeafb6
Nils Henrik Lorentzen <nils@erus.no>
2020-04-12 19:53:46 +0200

    New BaseImportsModel class

--- compiler-resolver-ast-objects/src/main/java/com/neaterbits/compiler/resolver/ast/objects/model/ObjectImportsModel.java
+++ compiler-resolver-ast-objects/src/main/java/com/neaterbits/compiler/resolver/ast/objects/model/ObjectImportsModel.java
@@ -5,26 +5,22 @@ import java.util.List;
 import com.neaterbits.compiler.ast.objects.CompilationUnit;
 import com.neaterbits.compiler.ast.objects.Import;
 import com.neaterbits.compiler.util.imports.TypeImport;
+import com.neaterbits.compiler.util.model.BaseImportsModel;
 import com.neaterbits.compiler.util.model.ImportsModel;
 import com.neaterbits.compiler.util.model.TypeImportVisitor;
 
-public class ObjectImportsModel implements ImportsModel<CompilationUnit> {
+public class ObjectImportsModel
+    extends BaseImportsModel<CompilationUnit>
+    implements ImportsModel<CompilationUnit> {
 
-	private final List<TypeImport> implicitImports;
-	
 	public ObjectImportsModel(List<TypeImport> implicitImports) {
-		this.implicitImports = implicitImports;
-	}
+        super(implicitImports);
+    }
 
-	@Override
+    @Override
 	public final void iterateTypeImports(CompilationUnit sourceFile, TypeImportVisitor visitor) {
 		for (Import typeImport : sourceFile.getImports()) {
 			typeImport.visit(visitor);
 		}
-	}
-
-	@Override
-	public final List<TypeImport> getImplicitImports() {
-		return implicitImports;
 	}
 }
