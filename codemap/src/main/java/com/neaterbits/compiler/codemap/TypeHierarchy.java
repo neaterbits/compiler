ab2d129e38d947b48ed22300488e7a4953c91de3
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2020-09-23 10:04:51 +0200

    COM-10 TypeHierarchy test

--- codemap/src/main/java/com/neaterbits/compiler/codemap/TypeHierarchy.java
+++ codemap/src/main/java/com/neaterbits/compiler/codemap/TypeHierarchy.java
@@ -14,9 +14,9 @@ import com.neaterbits.compiler.codemap.Encode.TypeTest;
 final class TypeHierarchy {
 
 	private int typeSequenceNo;
-	
+
 	private TypeVariant [] typeVariant;
-	
+
 	private int [][] thisExtendsFromEncoded;
 	private int [][] extendingFromThisEncoded;
 
@@ -32,15 +32,15 @@ final class TypeHierarchy {
 		this.extendingFromThisEncoded  = allocateIntArray(this.extendingFromThisEncoded,  numTypes);
 
 		this.typeVariant[typeNo] = typeVariant;
-		
+
 		if (    thisExtendsFromClassesEncoded != null && thisExtendsFromClassesEncoded.length != 0
 		     && thisExtendsFromInterfacesEncoded != null && thisExtendsFromInterfacesEncoded.length != 0) {
-			
-			
+
+
 			final int extendsFromLength = thisExtendsFromClassesEncoded.length + thisExtendsFromInterfacesEncoded.length;
-			
+
 			this.thisExtendsFromEncoded[typeNo] = new int[extendsFromLength];
-			
+
 			for (int i = 0; i < thisExtendsFromClassesEncoded.length; ++ i) {
 				this.thisExtendsFromEncoded[typeNo][i] = thisExtendsFromClassesEncoded[i];
 			}
@@ -50,23 +50,20 @@ final class TypeHierarchy {
 							= thisExtendsFromInterfacesEncoded[i];
 			}
 
-			setExtendedByOnBaseClasses(typeNo, typeVariant, thisExtendsFromClassesEncoded);
+			setExtendedBy(typeNo, typeVariant, thisExtendsFromClassesEncoded);
+            setExtendedBy(typeNo, typeVariant, thisExtendsFromInterfacesEncoded);
 		}
 		else if (thisExtendsFromClassesEncoded != null && thisExtendsFromClassesEncoded.length != 0) {
-		
+
 			this.thisExtendsFromEncoded[typeNo] = thisExtendsFromClassesEncoded;
-			
-			setExtendedByOnBaseClasses(typeNo, typeVariant, thisExtendsFromClassesEncoded);
+
+			setExtendedBy(typeNo, typeVariant, thisExtendsFromClassesEncoded);
 		}
 		else if (thisExtendsFromInterfacesEncoded != null && thisExtendsFromInterfacesEncoded.length != 0) {
-			
-			this.thisExtendsFromEncoded[typeNo] = thisExtendsFromInterfacesEncoded;
-			
-			for (int i = 0; i < thisExtendsFromInterfacesEncoded.length; ++ i) {
-				final int extendedTypeNo = Encode.decodeTypeNo(thisExtendsFromInterfacesEncoded[i]);
 
-				setExtendedBy(extendedTypeNo, typeNo, typeVariant);
-			}
+			this.thisExtendsFromEncoded[typeNo] = thisExtendsFromInterfacesEncoded;
+
+            setExtendedBy(typeNo, typeVariant, thisExtendsFromInterfacesEncoded);
 		}
 		else {
 			this.thisExtendsFromEncoded[typeNo] = null;
@@ -82,9 +79,9 @@ final class TypeHierarchy {
 
 	/*
 	private static int [] decodeTypes(int [] encodedTypes) {
-		
+
 		final int [] result;
-		
+
 		if (encodedTypes == null) {
 			result = null;
 		}
@@ -95,108 +92,113 @@ final class TypeHierarchy {
 				result[i] = Encode.decodeTypeNo(encodedTypes[i]);
 			}
 		}
-		
+
 		return result;
 	}
 	*/
-	
-	private void setExtendedByOnBaseClasses(int typeNo, TypeVariant typeVariant, int [] thisExtendsFromClassesEncoded) {
-		for (int extendedTypeNoEncoded : thisExtendsFromClassesEncoded) {
 
-			// System.out.println("## set extended by " + typeNo + " for " + decodeTypeNo(extendedTypeNoEncoded));
-			
+	private void setExtendedBy(int typeNo, TypeVariant typeVariant, int [] thisExtendsFromEncoded) {
+
+	    for (int extendedTypeNoEncoded : thisExtendsFromEncoded) {
 			setExtendedBy(decodeTypeNo(extendedTypeNoEncoded), typeNo, typeVariant);
 		}
 	}
-	
+
 	TypeVariant getTypeVariantForType(int typeNo) {
 		return typeVariant[typeNo];
 	}
-	
+
 	private void setExtendedBy(int extendedTypeNo, int typeNo, TypeVariant typeVariant) {
 
 		addToSubIntArray(this.extendingFromThisEncoded, extendedTypeNo, encodeType(typeNo, typeVariant), 5);
 
 	}
-	
+
 	int getExtendsFromSingleSuperClass(int typeNo) {
 		final int encoded = getExtendsFromSingleSuperClassEncoded(typeNo);
-		
+
 		return encoded != -1 ? Encode.decodeTypeNo(encoded) : -1;
 	}
-		
+
 	private int getExtendsFromSingleSuperClassEncoded(int typeNo) {
+
+	    if (typeVariant[typeNo] != TypeVariant.CLASS) {
+	        throw new IllegalArgumentException("Expected class");
+	    }
+
 		final int [] extendsFromEncodedArray = this.thisExtendsFromEncoded[typeNo];
-		
+
 		int found = -1;
-		
+
 		if (extendsFromEncodedArray != null) {
 			for (int extendsFromEncoded : extendsFromEncodedArray) {
 				if (Encode.isClass(extendsFromEncoded)) {
 					if (found != -1) {
 						throw new IllegalStateException();
 					}
-					
+
 					found = extendsFromEncoded;
 				}
 			}
 		}
-		
+
 		return found;
 	}
 
 	int [] getTypesThisExtendsFromEncoded(int typeNo) {
 		return thisExtendsFromEncoded[typeNo];
 	}
-	
+
 	int getNumExtendingThis(int typeNo) {
 		final int [] extendedBy = extendingFromThisEncoded[typeNo];
 
 		return extendedBy != null ? extendedBy[0] : 0;
 	}
-	
-	int getTypesExtendingThisEncoded(int typeNo, int idx) {
-		return extendingFromThisEncoded[typeNo][idx + 1];
-	}
-	
+
 	int [] getTypesExtendingThisEncoded(int typeNo) {
-		
+
 		final int [] extendingFromThis = extendingFromThisEncoded[typeNo];
 
 		return extendingFromThis != null ? subIntArrayCopy(extendingFromThis) : null;
 	}
-	
+
 	int [] getTypesThisExtendsFrom(int typeNo, TypeTest typeTest) {
 
 		final int [] extendsFromEncoded = this.thisExtendsFromEncoded[typeNo];
 
 		final int [] resultTypes;
-		
+
 		if (extendsFromEncoded != null) {
-		
+
 			int numEntries;
-			
+
 			if (typeTest == null) {
 				numEntries = extendsFromEncoded.length;
 			}
 			else {
 				numEntries = 0;
-				
+
 				for (int i = 0; i < extendsFromEncoded.length; ++ i) {
 					if (typeTest.onTypeNoEncoded(extendsFromEncoded[i])) {
 						++ numEntries;
 					}
 				}
 			}
-			
-			resultTypes = new int[numEntries];
-			
-			int dstIdx = 0;
-			
-			for (int i = 0; i < extendsFromEncoded.length; ++ i) {
-				if (typeTest == null || typeTest.onTypeNoEncoded(extendsFromEncoded[i])) {
-					resultTypes[dstIdx ++] = decodeTypeNo(extendsFromEncoded[i]);
-				}
+
+			if (numEntries > 0) {
+
+    			resultTypes = new int[numEntries];
+
+    			int dstIdx = 0;
+
+    			for (int i = 0; i < extendsFromEncoded.length; ++ i) {
+    				if (typeTest == null || typeTest.onTypeNoEncoded(extendsFromEncoded[i])) {
+    					resultTypes[dstIdx ++] = decodeTypeNo(extendsFromEncoded[i]);
+    				}
+    			}
+			}
+			else {
+			    resultTypes = null;
 			}
 		}
 		else {
@@ -208,15 +210,15 @@ final class TypeHierarchy {
 
 	int [] encodeTypeVariant(int [] types) {
 		final int [] result = new int[types.length];
-		
+
 		for (int i = 0; i < result.length; ++ i) {
 			final int extendsFromType = types[i];
-			
+
 			result[i] = Encode.encodeType(
 					extendsFromType,
 					getTypeVariantForType(extendsFromType));
 		}
-		
+
 		return result;
 	}
 }
