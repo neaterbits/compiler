797dbd7a873be0aad7225d289afb7329a12d1296
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2020-09-23 08:51:47 +0200

    StaticMethodOverrideMap test

--- codemap/src/main/java/com/neaterbits/compiler/codemap/DynamicMethodOverrideMap.java
+++ codemap/src/main/java/com/neaterbits/compiler/codemap/DynamicMethodOverrideMap.java
@@ -3,7 +3,7 @@ package com.neaterbits.compiler.codemap;
 public final class DynamicMethodOverrideMap extends MethodOverrideMap {
 
 	@Override
-	void addTypeExtendsTypes(int extendingTypeEncoded, int[] extendedTypesEncoded, MethodMap methodMap) {
+	void addTypeExtendsTypes(int extendingTypeEncoded, GetExtendedTypesEncoded getExtendedTypesEncoded, MethodMap methodMap) {
 		throw new UnsupportedOperationException();
 	}
 }
--- codemap/src/main/java/com/neaterbits/compiler/codemap/StaticMethodOverrideMap.java
+++ codemap/src/main/java/com/neaterbits/compiler/codemap/StaticMethodOverrideMap.java
@@ -10,38 +10,80 @@ import static com.neaterbits.compiler.codemap.Encode.getTypeVariant;
 public final class StaticMethodOverrideMap extends MethodOverrideMap {
 
 	@Override
-	void addTypeExtendsTypes(int extendingTypeEncoded, int [] extendedTypesEncoded, MethodMap methodMap) {
-		
+	void addTypeExtendsTypes(int extendingTypeEncoded, GetExtendedTypesEncoded getExtendedTypesEncoded, MethodMap methodMap) {
+
 		final int extendingType = decodeTypeNo(extendingTypeEncoded);
 		final int [] extendingMethodsEncoded = methodMap.getMethodsForTypeEncoded(extendingType);
-		
-		for (int i = subIntArrayInitialIndex(extendingMethodsEncoded); i <= subIntArrayLastIndex(extendingMethodsEncoded); ++ i) {
-			
+
+		final int initial = subIntArrayInitialIndex(extendingMethodsEncoded);
+		final int last = subIntArrayLastIndex(extendingMethodsEncoded);
+
+		for (int i = initial; i <= last; ++ i) {
+
 			final int extendingMethodEncoded = extendingMethodsEncoded[i];
-			
+
 			final int extendingMethod = decodeMethodNo(extendingMethodEncoded);
-			
+
 			final int methodSignatureNo = methodMap.getMethodSignature(extendingMethod);
-			
-			for (int extendedTypeEncoded : extendedTypesEncoded) {
-				
-				final int extendedType = decodeTypeNo(extendedTypeEncoded);
 
-				final int extendedMethodNo = 
-						methodMap.getMethodNoBySignatureNo(extendedType, methodSignatureNo);
-				
-				if (extendedMethodNo != -1) {
-					final TypeVariant extendedTypeVariant = getTypeVariant(extendedTypeEncoded);
-
-					addMethodExtends(
-							encodeMethodWithMethodVariant(
-									(int)extendedMethodNo,
-									extendedTypeVariant),
-							
-							extendingMethodEncoded);
-				}
-			}
+			addFromExtendedTypes(
+			        extendingMethodEncoded,
+			        methodSignatureNo,
+			        extendingType,
+			        getExtendedTypesEncoded,
+			        methodMap);
 		}
 	}
 
+	private boolean addFromExtendedTypes(
+	        int extendingMethodEncoded,
+	        int methodSignatureNo,
+	        int typeNo,
+	        GetExtendedTypesEncoded getExtendedTypesEncoded,
+	        MethodMap methodMap) {
+
+	    final int [] extendedTypesEncoded = getExtendedTypesEncoded.getTypes(typeNo);
+
+	    boolean added = false;
+
+	    if (extendedTypesEncoded != null) {
+
+	        for (int extendedTypeEncoded : extendedTypesEncoded) {
+
+                final int extendedType = decodeTypeNo(extendedTypeEncoded);
+
+                final int extendedMethodNo =
+                        methodMap.getMethodNoBySignatureNo(extendedType, methodSignatureNo);
+
+                if (extendedMethodNo != -1) {
+                    final TypeVariant extendedTypeVariant = getTypeVariant(extendedTypeEncoded);
+
+                    addMethodExtends(
+                            encodeMethodWithMethodVariant(
+                                    extendedMethodNo,
+                                    extendedTypeVariant),
+
+                            extendingMethodEncoded);
+
+                    added = true;
+                    break;
+                }
+                else {
+
+                    added = addFromExtendedTypes(
+                            extendingMethodEncoded,
+                            methodSignatureNo,
+                            extendedType,
+                            getExtendedTypesEncoded,
+                            methodMap);
+
+                    if (added) {
+                        break;
+                    }
+                }
+            }
+	    }
+
+	    return added;
+	}
 }
