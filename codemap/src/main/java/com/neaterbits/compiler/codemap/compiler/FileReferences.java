c7b64e71abcea07c0644e86863f47110261ce167
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2020-09-23 17:59:35 +0200

    COM-12 FileReferences test

--- codemap/src/main/java/com/neaterbits/compiler/codemap/compiler/FileReferences.java
+++ codemap/src/main/java/com/neaterbits/compiler/codemap/compiler/FileReferences.java
@@ -7,32 +7,41 @@ import java.util.Arrays;
 public final class FileReferences {
 
 	private static final int [] EMPTY_ARRAY = new int[0];
-	
+
 	private int [] fileByType;
 	private int [][] typesByFile;
-	
-	public int addFile(int fileNo, int [] types) {
+
+	int addFile(int fileNo, int [] types) {
 
 		this.typesByFile = allocateIntArray(this.typesByFile, fileNo + 1);
-		
+
 		if (typesByFile[fileNo] != null) {
 			throw new IllegalStateException();
 		}
-		
+
 		if (types != null) {
 			typesByFile[fileNo] = Arrays.copyOf(types, types.length);
-			
+
 			for (int type : types) {
 				this.fileByType = allocateIntArray(fileByType, type + 1);
-				
+
 				this.fileByType[type] = fileNo;
 			}
 		}
 		else {
 			typesByFile[fileNo] = EMPTY_ARRAY;
 		}
-		
+
 		return fileNo;
+	}
+
+	int [] getTypesForFile(int fileNo) {
+
+	    if (fileNo >= typesByFile.length) {
+	        throw new IllegalArgumentException();
+	    }
+
+	    return typesByFile[fileNo];
 	}
 
 	void removeFile(int fileNo) {
@@ -40,7 +49,7 @@ public final class FileReferences {
 		for (int type : typesByFile[fileNo]) {
 			fileByType[type] = IntCompilerCodeMap.SOURCEFILE_UNDEF;
 		}
-		
+
 		typesByFile[fileNo] = null;
 	}
 }
