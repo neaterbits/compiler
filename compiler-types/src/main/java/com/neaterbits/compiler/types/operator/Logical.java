7a78ed64c68abeafe1b423f5aa4e40ca71dd1bc9
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2021-01-03 15:09:36 +0100

    COM-32 logical operator NOT is UnaryOperator

--- compiler-types/src/main/java/com/neaterbits/compiler/types/operator/Logical.java
+++ compiler-types/src/main/java/com/neaterbits/compiler/types/operator/Logical.java
@@ -1,6 +1,6 @@
 package com.neaterbits.compiler.types.operator;
 
-public enum Logical implements Operator {
+public enum Logical implements Operator, UnaryOperator {
 	AND(Arity.BINARY),
 	OR(Arity.BINARY),
 	NOT(Arity.UNARY);
@@ -27,6 +27,21 @@ public enum Logical implements Operator {
 	}
 
 	@Override
+    public Notation getNotation() {
+
+	    final Notation notation;
+	    
+	    if (this == NOT) {
+	        notation = Notation.PREFIX;
+	    }
+	    else {
+	        throw new UnsupportedOperationException();
+	    }
+
+	    return notation;
+    }
+
+    @Override
 	public <T, R> R visit(OperatorVisitor<T, R> visitor, T param) {
 		return visitor.onLogical(this, param);
 	}
