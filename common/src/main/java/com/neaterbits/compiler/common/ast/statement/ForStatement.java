e59cb7a0651a7d0fd4a28b4d69b7b8f6bf9badda
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2021-02-20 16:38:42 +0100

    Inherit ForStatement from LoopStatement

--- common/src/main/java/com/neaterbits/compiler/common/ast/statement/ForStatement.java
+++ common/src/main/java/com/neaterbits/compiler/common/ast/statement/ForStatement.java
@@ -1,7 +1,5 @@
 package com.neaterbits.compiler.common.ast.statement;
 
-import java.util.Objects;
-
 import com.neaterbits.compiler.common.Context;
 import com.neaterbits.compiler.common.ast.ASTRecurseMode;
 import com.neaterbits.compiler.common.ast.ASTVisitor;
@@ -9,24 +7,18 @@ import com.neaterbits.compiler.common.ast.block.Block;
 import com.neaterbits.compiler.common.ast.expression.Expression;
 import com.neaterbits.compiler.common.ast.list.ASTSingle;
 
-public final class ForStatement extends Statement {
+public final class ForStatement extends LoopStatement {
 
 	private final ASTSingle<ForInit> forInit;
 	private final ASTSingle<Expression> condition;
 	private final ASTSingle<ForExpressionList> forUpdate;
-	
-	private final ASTSingle<Block> block;
 
 	public ForStatement(Context context, ForInit forInit, Expression condition, ForExpressionList forUpdate, Block block) {
-		super(context);
-		
-		Objects.requireNonNull(block);
+		super(context, block);
 		
 		this.forInit   = forInit   != null ? makeSingle(forInit)   : null;
 		this.condition = condition != null ? makeSingle(condition) : null;
 		this.forUpdate = forUpdate != null ? makeSingle(forUpdate) : null;
-
-		this.block = makeSingle(block);
 	}
 
 	public ForInit getForInit() {
@@ -39,10 +31,6 @@ public final class ForStatement extends Statement {
 
 	public ForExpressionList getForUpdate() {
 		return forUpdate != null ? forUpdate.get() : null;
-	}
-
-	public Block getBlock() {
-		return block.get();
 	}
 
 	@Override
